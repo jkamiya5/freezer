7cad55c9225880a4e000f8fdb6bb4a83d79d0dac
wordconverter <kamiyaj@stationp.co.jp>
2017-01-05 21:43:40 +0900

    実装

---
+++ css/myCss.cs
@@ -0,0 +1,17 @@
+.sample01 {
+ float: left;
+ width: 200px;
+ border: 1px solid #333333;
+ padding: 5px;
+ margin: 0px 0px 10px 0px;
+}
+.sample02 {
+ width: 200px;
+ padding: 5px;
+ margin: 10px 10px 10px 700px;
+}
+.sample03 {
+ clear: both;
+ border: 1px solid #333333;
+ padding: 5px;
+}
